cbd1c4e4e774b54546289a8e48aeecb82691b4c5
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-29 17:45:08 +0100

    Working

--- handlebars/script.js
+++ handlebars/script.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
+
+  $(document).on('click', '.button', function() {
+    console.log($('.input').val());
+  });
+
   // var source = document.getElementById("template").innerHTML;
-  var source = $('.template').html();
+  var source = $('#template').html();
   var template = Handlebars.compile(source);
+
+  var context = {
+    reply : 'hello world ciao mondo'
+  };
+
+  var html = template(context);
+
+  $('.container').append(html);
 
 });
